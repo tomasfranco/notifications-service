2bc50280f822cf524c07882160d33b67a4b825eb
Tomas Franco <tomas.f.carvalho@gmail.com>
2022-12-18 11:49:26 -0300

    Ignite Labb - NodeJS - Aula 3 concluida com sucesso

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
-import { SendNotification } from './../../application/use-cases/send-notification';
 import { Module } from '@nestjs/common';
+import { SendNotification } from '@application/use-cases/send-notification';
+import { CancelNotification } from './../../application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from './../../application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from './../../application/use-cases/get-recipient-notifications';
+import { ReadNotification } from './../../application/use-cases/read-notification';
+import { UnreadNotification } from './../../application/use-cases/unread-notification';
 import { NotificationsController } from './controllers/notifications.controller';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
